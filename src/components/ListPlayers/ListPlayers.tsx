d731884e7f5b2c55722d6e33032527e84e9d1bfb
FranderCT <09.reelaborar-cerosa@icloud.com>
2025-09-18 03:30:09 -0600

    cambios

---
+++ src/components/ListPlayers/ListPlayers.tsx
@@ -0,0 +1,33 @@
+// src/components/Lobby/ListPlayers.tsx
+import type { Player } from "../../models/Player";
+import { User } from "lucide-react"; // icono simple de usuario
+
+type Props = {
+  players: Player[];
+ 
+};
+
+export default function ListPlayers({ players }: Props) {
+  return (
+    <div className="w-full rounded-lg p-4 text-white">
+     
+      {/* igual que en TopGlobals → alto fijo + scroll */}
+      <div className="mt-4 flex h-64 flex-col gap-3 overflow-y-auto pr-2 nice-scroll">
+        {players.map((p) => (
+          <div
+            key={p.Id}
+            className="flex items-center justify-between rounded-lg bg-[#22232b] px-4 py-3 shadow-md ring-1 ring-white/10"
+          >
+            <div className="flex items-center gap-2">
+              <User className="h-5 w-5 text-indigo-400" />
+              <span className="text-white/90">
+                {p.NamePlayer}
+              </span>
+            </div>
+            
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -4,6 +4,7 @@ import binariosFoto from "../assets/binariosFoto.png";
 import PlayerHost from "../components/Lobby/RoomInfo/PlayerHost";
 import RoomCode from "../components/Lobby/RoomInfo/RoomCode";
 import type { Room } from "../models/Room";
+import ListPlayers from "../components/ListPlayers/ListPlayers";
 
 const playersData: Player[] = [
   { Id: 1, NamePlayer: "Frander", TurnOrder: 1, Position: 5, Wins: 3 },
@@ -16,6 +17,19 @@ const playersData: Player[] = [
 const playerhost: Player = { Id: 1, NamePlayer: "Frander", TurnOrder: 1, Position: 5, Wins: 3 };
 
 const roomCode : Room = {IdRoom: 1, Code: "ABC-123"}
+
+const playerssData: Player[] = [
+  { Id: 1, NamePlayer: "Frander", Wins: 3 },
+  { Id: 2, NamePlayer: "Brenda",  Wins: 5 },
+  { Id: 3, NamePlayer: "Luis",    Wins: 1 },
+  { Id: 4, NamePlayer: "Samuel",  Wins: 2 },
+  { Id: 5, NamePlayer: "Katherine", Wins: 4 },
+  { Id: 6, NamePlayer: "José", Wins: 0 },
+  { Id: 7, NamePlayer: "María", Wins: 7 },
+  { Id: 8, NamePlayer: "Pedro", Wins: 2 },
+  { Id: 9, NamePlayer: "Ana", Wins: 1 },
+];
+
 
 export default function Lobby() {
   return (
@@ -45,6 +59,22 @@ export default function Lobby() {
         <section className="rounded-2xl border border-white/10 bg-white/5 p-4 sm:p-6 shadow-2xl shadow-indigo-900/10 flex flex-col md:flex-row items-center justify-between gap-4">
             <PlayerHost player={playerhost} />
             <RoomCode room={roomCode} />
+        </section>
+
+        <section className="rounded-2xl w-full max-w-5xl px-4 sm:px-6 border border-white/10 bg-white/5  lg:px- py-3 flex flex-col items-center justify-center">
+            <span className="text-2xl font-semibold text-center text-white/90">
+                Jugadores en la Sala
+            </span>
+            <ListPlayers players={playerssData} />
+            <button
+                onClick={() => {}}
+                className="px-6 py-4 rounded-xl bg-gradient-to-r from-emerald-500 to-emerald-600 
+                        text-white font-semibold text-lg shadow-lg shadow-emerald-700/30
+                        hover:brightness-110 active:scale-95 transition"
+            >
+                Iniciar Partida
+            </button>
+            
         </section>
 
       </div>
